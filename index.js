cb2d7655c616d0c95447959096c463eb81b38dd3
adarsh sharma <adarshpurushottam2002@gmail.com>
2025-12-17 17:27:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const aboutButton = document.getElementById("about-button");
+const registerButton=document.getElementById("a")
+aboutButton.addEventListener("click", () => {
+ 
+  window.location.replace("./pages/about.html");
+});
+registerButton.addEventListener("Register", () => {
+  
+  window.location.replace("./pages/register.html");
+});
---
+++ javascript-pages/shop.js
@@ -0,0 +1,21 @@
+import { data } from "../data/data.js";
+
+const addData = document.getElementById("data");
+
+// data.map((i) => {
+//   const x = document.createElement("h3");
+//   const y = document.createElement("p");
+//   const image = document.createElement("img");
+//   const grp = document.createElement("div");
+//   grp.classList.add("arrange")
+//   x.innerText = i.title;
+//   y.innerText = i.description;
+//   image.src = i.image;
+  
+//   grp.appendChild(x);
+//   grp.appendChild(y);
+//   grp.appendChild(image);
+//   addData.append(grp);
+// });
+
+console.log(data);
